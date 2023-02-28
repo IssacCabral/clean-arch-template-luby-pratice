dacd1176a7253d4fc3a67995bf98523a72fa722e
Issac Cabral <issaccabrall@gmail.com>
2023-02-28 09:02:28 -0300

    chore: define creat user use case

---
+++ src/2-business/module/errors/users/usersErrors.ts
@@ -0,0 +1,52 @@
+import { IError } from "@shared/IError";
+
+export class UsersErrors extends IError {
+  static entityCreationError(): IError {
+    const userErrors = new UsersErrors({
+      statusCode: 400,
+      body: {
+        code: "UE-001",
+        message:
+          "Error during creation of the user entity, please try again later",
+        shortMessage: "entityCreationFailed",
+      },
+    });
+    return userErrors;
+  }
+
+  static userEmailAlreadyInUse(): IError {
+    const userErrors = new UsersErrors({
+      statusCode: 400,
+      body: {
+        code: "UE-002",
+        message: "This e-mail is already in use, please use another",
+        shortMessage: "entityCreationFailed",
+      },
+    });
+    return userErrors;
+  }
+
+  static userNotFound(): IError {
+    const userErrors = new UsersErrors({
+      statusCode: 404,
+      body: {
+        code: "UE-003",
+        message: "User not found",
+        shortMessage: "useNotFound",
+      },
+    });
+    return userErrors;
+  }
+
+  static userNotLoadedCorrectly(): IError {
+    const userErrors = new UsersErrors({
+      statusCode: 400,
+      body: {
+        code: "UE-004",
+        message: "User entity not loaded as espected",
+        shortMessage: "userNotLoadedCorrectly",
+      },
+    });
+    return userErrors;
+  }
+}
--- src/2-business/usecases/user/createUserUseCase.ts
+++ src/2-business/usecases/user/createUserUseCase.ts
@@ -1,3 +1,54 @@
 import { IAbstractUseCase } from "../abstractUseCase";
+import {
+  IInputCreateUserDto,
+  IOutputCreateUserDto,
+} from "@business/dto/user/create";
+import { inject, injectable } from "inversify";
+import {
+  IUserRepository,
+  IUserRepositoryToken,
+} from "@business/repositories/user/iUserRepository";
+import {
+  IHasherService,
+  IHasherServiceToken,
+} from "@business/services/hasher/iHasher";
+import {
+  IUniqueIdentifierService,
+  IUniqueIdentifierServiceToken,
+} from "@business/services/uniqueIdentifier/iUniqueIdentifier";
+import { UserEntity } from "@domain/entities/userEntity";
+import { left, right } from "@shared/either";
+import { UsersErrors } from "@business/module/errors/users/usersErrors";
 
-export class CreateUserUseCase implements IAbstractUseCase
+@injectable()
+export class CreateUserUseCase
+  implements IAbstractUseCase<IInputCreateUserDto, IOutputCreateUserDto>
+{
+  constructor(
+    @inject(IUserRepositoryToken) private userRepository: IUserRepository,
+    @inject(IHasherServiceToken) private hasherService: IHasherService,
+    @inject(IUniqueIdentifierServiceToken)
+    private uniqueIdentifierService: IUniqueIdentifierService
+  ) {}
+
+  async exec(input: IInputCreateUserDto): Promise<IOutputCreateUserDto> {
+    try {
+      const hashPassword = await this.hasherService.create(input.password);
+
+      const createUser = UserEntity.create({
+        ...input,
+        password: hashPassword,
+      });
+
+      const user = {
+        ...createUser.value.export(),
+        uuid: this.uniqueIdentifierService.create(),
+      };
+
+      const userEntity = await this.userRepository.create(user, input.role_id);
+      return right(userEntity);
+    } catch (error) {
+      return left(UsersErrors.entityCreationError());
+    }
+  }
+}
