57f624def91c0adbbfa824641b5aac645a761824
Issac Cabral <issaccabrall@gmail.com>
2023-03-06 13:31:20 -0300

    chore: define product business errors

--- product_sqs/src/2-business/dto/product/create.ts
+++ product_sqs/src/2-business/dto/product/create.ts
@@ -1,5 +1,10 @@
+import { IProductEntity } from "@domain/entities/productEntity";
+import { Either } from "@shared/either";
+import { IError } from "@shared/IError";
+
 export interface IInputCreateProductDto {
-  profile: string;
+  name: string;
+  quantity: number;
 }
 
-export type IOutputCreateProductDto = Either<IError, IRoleEntity>;
+export type IOutputCreateProductDto = Either<IError, IProductEntity>;
---
+++ product_sqs/src/2-business/module/errors/products/productsErrors.ts
@@ -0,0 +1,42 @@
+import { IError } from "@shared/IError";
+
+export class ProductsErrors extends IError {
+  static productNotFound(): IError {
+    const productError = new ProductsErrors({
+      statusCode: 404,
+      body: {
+        code: "RE-001",
+        message: "The product acess that you were searching was not found",
+        shortMessage: "productNotFound",
+      },
+    });
+
+    return productError;
+  }
+
+  static productFailedToUpdate(): IError {
+    const productError = new ProductsErrors({
+      statusCode: 500,
+      body: {
+        code: "RE-003",
+        message: "product could not be updated",
+        shortMessage: "productNotUpdated",
+      },
+    });
+
+    return productError;
+  }
+
+  static productFailedToCreate(): IError {
+    const productError = new ProductsErrors({
+      statusCode: 500,
+      body: {
+        code: "RE-004",
+        message: "Product could not be created",
+        shortMessage: "productNotCreated",
+      },
+    });
+
+    return productError;
+  }
+}
